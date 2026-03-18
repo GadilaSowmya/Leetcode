5b02afa820a0915ff03e4d210edf7a5167a8b58b
GadilaSowmya <23211a0588@bvrit.ac.in>
2026-03-18 10:47:01 +0530

    Time: 5 ms (68.42%) | Memory: 46 MB (46.26%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -1,11 +1,15 @@
 class Solution {
     public boolean judgeCircle(String moves) {
         int x=0,y=0;
-        for(int i=0;i<moves.length();i++){
-            if(moves.charAt(i)=='U') x++;
-            else if(moves.charAt(i)=='D') x--;
-            else if (moves.charAt(i)=='R') y++;
-            else  y--;
+        for (char c : moves.toCharArray()) {
+            if (c == 'U')
+                y++;
+            else if (c == 'D')
+                y--;
+            else if (c == 'L')
+                x--;
+            else if (c == 'R')
+                x++;
         }
         return x == 0 && y == 0;
     }
