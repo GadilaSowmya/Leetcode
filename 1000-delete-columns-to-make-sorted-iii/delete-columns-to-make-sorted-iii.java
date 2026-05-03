74cc4724e209e25ec0bc687f59942e7bc4714d77
GadilaSowmya <23211a0588@bvrit.ac.in>
2026-05-03 20:11:31 +0530

    Time: 6 ms (99.59%) | Memory: 43.9 MB (94.63%) - LeetSync

---
+++ 1000-delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int n = strs.length;
+        int m = strs[0].length();
+
+        int[] dp = new int[m];
+        int maxKept = 1;
+
+        for (int j = 0; j < m; j++) {
+            dp[j] = 1;
+            for (int i = 0; i < j; i++) {
+                if (isCompatible(strs, i, j)) {
+                    dp[j] = Math.max(dp[j], dp[i] + 1);
+                }
+            }
+            maxKept = Math.max(maxKept, dp[j]);
+        }
+
+        return m - maxKept;
+    }
+
+    private boolean isCompatible(String[] strs, int i, int j) {
+        for (String s : strs) {
+            if (s.charAt(i) > s.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
